567183e061ce9f0680844e67c38615eef2b6b021
Suraj Jagdev <suraj_57@hotmail.com>
2019-02-17 19:04:50 -0500

    having back-end server serving react front-end

---
+++ server.js
@@ -0,0 +1,27 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+//serve up static assets production
+if (process.env.NODE_ENV === 'prodution') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join((__dirname = './client/build/index.html')));
+    /*const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);*/
+  });
+}
+//build mode local
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+});
+
+//use routes when made and connect to mongoose
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
