13d9d6a6c231d31b28e1df6ecafac39a8fe7b2cd
Suraj Jagdev <suraj_57@hotmail.com>
2019-02-23 15:42:06 -0500

    changed user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
 //export
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
-    //user's name
-    name: {
+    //first name
+    first_name: {
       //String limit of 255 default, making it 50 length
       type: DataTypes.STRING({ length: 50 }),
       //not allowing empty
@@ -16,8 +16,40 @@ module.exports = function(sequelize, DataTypes) {
           msg: 'Name must be between 1 and 50 characters'
         }
       }
+    },
+    //last name
+    last_name: {
+      //String limit of 255 default, making it 50 length
+      type: DataTypes.STRING({ length: 50 }),
+      //not allowing empty
+      allowNull: false,
+      //validation
+      validation: {
+        //is letters only
+        is: { args: ['^[a-z]+$', 'i'], msg: 'Name must be letters only' },
+        len: {
+          args: [1, 50],
+          msg: 'Name must be between 1 and 50 characters'
+        }
+      }
+    },
+    //email
+    email: {
+      //String limit of 255 default, making it 100 max length
+      type: DataTypes.STRING({ length: 100 }),
+      //not allowing empty
+      allowNull: false,
+      //validation
+      validation: {
+        //is letters only
+        isEmail: true,
+        msg: 'Email Formatted Incorrectly'
+      },
+      len: {
+        args: [1, 100],
+        msg: 'Length is invalid'
+      }
     }
-    //column name
   });
   return User;
 };
