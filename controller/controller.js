13452d75a134d9b1240eb287967a910e0cc4ff14
Suraj Jagdev <suraj_57@hotmail.com>
2019-06-04 16:04:28 -0400

    added console log and moved passport seralize/deseralize fns to new dir

--- controller/controller.js
+++ controller/controller.js
@@ -4,7 +4,7 @@ const passport = require('passport');
 const express = require('express');
 const router = express.Router();
 const saltRounds = 10;
-router.get('/api/users', (req, res, next) => {
+router.get('/api/users', authenticationMiddleware(), (req, res, next) => {
   db.User.findAll({})
     .then(dbFindAll => {
       return res.json(dbFindAll);
@@ -15,66 +15,93 @@ router.get('/api/users', (req, res, next) => {
       }
     });
 });
-router.post('/api/newuser', (req, res, next) => {
-  //strip tags afterwards.
-  const { first_name, last_name, email, password } = req.body;
-  //strip html tags and remove uneccessary white spaces
-  const stripTagsFunction = myString => {
-    return myString.replace(/(<([^>]+)>)/gi, '');
-  };
-  if (first_name && last_name && email && password) {
-    bycrpt.hash(password, saltRounds, function(err, hash) {
-      if (!err) {
-        db.User.create({
-          first_name: stripTagsFunction(first_name.split(' ').join('')),
-          last_name: stripTagsFunction(last_name.split(' ').join('')),
-          email: stripTagsFunction(email.split(' ').join('')),
-          password: hash
-        })
-          .then(created => {
-            const userId = created.id;
-            if (!created) {
-              return res.status(400).json({
-                success: false,
-                message: 'An error has occured. User has not been saved'
-              });
-            } else if (created && typeof userId !== 'undefined') {
-              req.login(userId, {}, error => {
-                // console.log('req.user: ', req);
-                console.log('hi: ', req.session.passport.user);
-                console.log('is authenticated: ', req.isAuthenticated());
-                if (!error) {
-                  return res.json({
-                    success: true,
-                    data: created,
-                    message: 'User successfully created'
-                  });
-                } else {
-                  return res.json({
-                    success: false,
-                    data: 'corrupted seralization',
-                    message: 'User created Successfully, but not seralized'
-                  });
-                }
-              });
-            }
+router.post(
+  '/api/newuser',
+  checkAuthenticationMiddleware(),
+  (req, res, next) => {
+    //strip tags afterwards.
+    setTimeout(() => {
+      console.log(req.session);
+    }, 5000);
+    const { first_name, last_name, email, password } = req.body;
+    //strip html tags and remove uneccessary white spaces
+    const stripTagsFunction = myString => {
+      return myString.replace(/(<([^>]+)>)/gi, '');
+    };
+    if (first_name && last_name && email && password) {
+      bycrpt.hash(password, saltRounds, function(err, hash) {
+        if (!err) {
+          db.User.create({
+            first_name: stripTagsFunction(first_name.split(' ').join('')),
+            last_name: stripTagsFunction(last_name.split(' ').join('')),
+            email: stripTagsFunction(email.split(' ').join('')),
+            password: hash
           })
-          .catch(error => {
-            if (error) {
-              return res.json({ success: false, errors: error });
-            }
+            .then(created => {
+              const userId = created.id;
+              if (!created) {
+                return res.status(400).json({
+                  success: false,
+                  errors: {
+                    errors: [
+                      {
+                        message: 'An error has occured. User has not been saved'
+                      }
+                    ]
+                  },
+                  message: 'An error has occured. User has not been saved'
+                });
+              } else if (created && typeof userId !== 'undefined') {
+                req.login(userId, {}, error => {
+                  // console.log('req.user: ', req);
+                  if (!error) {
+                    console.log(req.session);
+                    return res.json({
+                      success: true,
+                      data: created,
+                      message: 'User successfully created'
+                    });
+                  } else {
+                    return res.json({
+                      success: false,
+                      errors: {
+                        errors: [{ message: 'corrupted seralization' }]
+                      },
+                      message: 'User created Successfully, but not seralized'
+                    });
+                  }
+                });
+              }
+            })
+            .catch(error => {
+              if (error) {
+                return res.json({
+                  success: false,
+                  errors: {
+                    errors: [{ message: JSON.stringify(error) }]
+                  },
+                  message: 'Error Caught on Server. Please Try again.'
+                });
+              }
+            });
+        } else {
+          res.json({
+            success: false,
+            errors: { errors: [{ error: err, message: 'Hash failed' }] },
+            message: 'Hash Failed'
           });
-      } else {
-        res.json({ success: false, errors: err, message: 'Hash Failed' });
-      }
-    });
-  } else {
-    res
-      .status(400)
-      .json({ success: false, message: 'Error Missing Parameters' });
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        errors: { errors: [{ message: 'Error Missing Parameters' }] },
+        message: 'Error Missing Parameters'
+      });
+    }
   }
-});
-router.get('/test', authenticationMiddleware(), (req, res) => {
+);
+router.get('/api/newuser/test', authenticationMiddleware(), (req, res) => {
   return res.send('hi');
 });
 passport.serializeUser(function(userId, done) {
@@ -100,52 +127,48 @@ function authenticationMiddleware() {
     if (req.isAuthenticated()) {
       return next();
     } else {
-      return res
-        .json({
-          success: false,
-          data: 'unable to login',
-          message: 'unsuccessfull login process'
-        })
-        .status(404);
-    }
-  };
-  /*return (req, res, next) => {
-    const user = req.signedCookies.user_sid;
-    console.log(req);
-    console.log('user: ', user);
-    db.Session.findOne({
-      where: {
-        sid: user
-      }
-    }).then(found => {
-      if (!found) {
-        console.log(found);
-        return res.json({
-          auth: req.isAuthenticated(),
-          message: 'User not Found. Not authenticated.'
-        });
-      } else {
-        return login(found.id);
-      }
-    });
-    function login(user) {
-      req.login(user, error => {
-        if (!error) {
-          return (
-            next(),
-            res.json({ auth: req.isAuthenticated(), message: 'authenticated' })
-          );
-        } else {
-          return res.json({
-            auth: req.isAuthenticated(),
-            user: req.user,
-            error: 'Error',
-            message: 'Auth middleware'
-          });
-        }
+      return res.json({
+        success: false,
+        error: true,
+        errors: {
+          errors: [
+            {
+              message: 'Unsuccessfull login authorized. Please Log back in.'
+            }
+          ]
+        },
+        message: 'unsuccessfull login process'
       });
     }
-  };*/
+  };
+}
+function checkAuthenticationMiddleware() {
+  return (req, res, next) => {
+    console.log(req.session);
+    console.log(req.cookies);
+    console.log(
+      'is authenticated via req.isAuthenticated: ',
+      req.isAuthenticated()
+    );
+    if (req.isAuthenticated()) {
+      return res.json({
+        success: false,
+        error: true,
+        errors: {
+          errors: [
+            {
+              message:
+                'User Already exists in Database. Cannot Register. Try Logging in.'
+            }
+          ]
+        },
+        message: 'Already Logged in'
+      });
+    } else {
+      console.log('returning next. User DNE');
+      return next();
+    }
+  };
 }
 
 //passport
