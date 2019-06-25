1194135695e7e79ef7515aa087ef32da1c7713c6
Suraj Jagdev <suraj_57@hotmail.com>
2019-06-25 15:14:16 -0400

    loading spinner return null is isLoading=false

--- client/src/loading/loading.js
+++ client/src/loading/loading.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './loading.css';
 const Loading = props => {
   if (props.isLoading === true) {
-    return <div className="loading" />;
+    return (
+      <div className="loading">
+        <h4>Loading...</h4>
+      </div>
+    );
   } else {
     return null;
   }
