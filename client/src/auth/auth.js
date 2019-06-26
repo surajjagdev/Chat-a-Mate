492d57b0cc8dc6ae8a4bb8c3eabbf3cd64092344
Suraj Jagdev <suraj_57@hotmail.com>
2019-06-26 16:35:28 -0400

    constructor vals firstName,lastName,email

--- client/src/auth/auth.js
+++ client/src/auth/auth.js
@@ -3,14 +3,27 @@ import API from '../utils/api.js';
 class Auth {
   constructor() {
     this.authenticated = this.checkAuth();
+    this.firstName = 'PlaceHolder';
+    this.lastName = 'PlaceHolder';
+    this.email = 'PlaceHolder';
   }
   checkAuth() {
     API.checkauth()
       .then(data => {
         if (data.data.success === true) {
-          return (this.authenticated = true);
+          return (
+            (this.authenticated = true),
+            (this.firstName = data.data.details.firstName),
+            (this.lastName = data.data.details.lastName),
+            (this.email = data.data.details.email)
+          );
         } else {
-          return (this.authenticated = false);
+          return (
+            (this.authenticated = false),
+            (this.firstName = 'Login'),
+            (this.lastName = 'Login'),
+            (this.email = 'login')
+          );
         }
       })
       .catch(error => {
@@ -34,6 +47,7 @@ class Auth {
   }
   isAuthenticated() {
     return this.authenticated;
+    //this.authenticated, this.firstName, this.lastName, this.email;
   }
 }
 export default new Auth();
