a5580c65dfdf1c3b965676306315ef93929916aa
Suraj Jagdev <suraj_57@hotmail.com>
2019-06-29 13:57:58 -0400

    css fixes for responsive header. Need to change to hamburger icon below 350px

--- client/src/banner/banner.js
+++ client/src/banner/banner.js
@@ -4,39 +4,31 @@ import './banner.css';
 import Logo from '../logo/logo.js';
 const Banner = props => {
   return (
-    <div className="Banner">
-      <div style={{ display: 'grid', gridTemplateColumns: '150px  1fr' }}>
+    <div className="banner">
+      <div className="logoandsearch">
         <Logo />
-        <input
-          className="searchBar"
-          type="text"
-          placeholder="Search"
-          name="searchbar"
-          onChange={e => {
-            props.handleInput(e);
-          }}
-        />
+        <div>
+          <input
+            className="searchBar"
+            type="text"
+            placeholder="Search"
+            name="searchbar"
+            onChange={e => {
+              props.handleInput(e);
+            }}
+          />
+          <button
+            className="searchButton"
+            onClick={e => {
+              console.log('hello');
+            }}
+          >
+            <i className="material-icons">search</i>
+          </button>
+        </div>
       </div>
-      <button
-        className="searchButton"
-        onClick={e => {
-          console.log('hello');
-        }}
-      >
-        <i className="material-icons">search</i>
-      </button>
-      <div />
-      <div
-        className="userLinks"
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '40px 40px 40px 40px 40px 40px',
-          gridGap: '5px',
-          marginLeft: '5px',
-          marginTop: '5px',
-          textAlign: 'center'
-        }}
-      >
+      <div className="emptyDiv" />
+      <div className="userLinks">
         <div className="bannerIcons">
           <i className="material-icons">home</i>
         </div>
