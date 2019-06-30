a1c84bcf24e1ccc2801be4139b00f32bdb4d1d42
Suraj Jagdev <suraj_57@hotmail.com>
2019-06-30 16:25:43 -0400

    adding profile satte width

--- client/src/profile/profile.js
+++ client/src/profile/profile.js
@@ -2,14 +2,18 @@ import React from 'react';
 import auth from '../auth/auth.js';
 import Banner from '../banner/banner.js';
 import { Link } from 'react-router-dom';
+import MobileMenuLinks from '../banner/mobilemenulinks.js';
 class Profile extends React.Component {
   state = {
     search: '',
     firstName: '',
     lastName: '',
-    email: ''
+    email: '',
+    sideDrawerOpen: false,
+    width: window.innerWidth
   };
   componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
     const details = () => {
       setTimeout(() => {
         if (auth.firstName !== 'PlaceHolder')
@@ -27,20 +31,48 @@ class Profile extends React.Component {
     };
     auth.checkAuth(details());
   }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
   handleInput = e => {
     e.preventDefault();
     this.setState({ search: e.target.value }, () => {
       console.log(this.state.search);
     });
   };
+  toggleClickHandler = e => {
+    e.preventDefault();
+    this.setState(
+      prevState => {
+        return { sideDrawerOpen: !prevState.sideDrawerOpen };
+      },
+      () => {
+        console.log(this.state.sideDrawerOpen);
+      }
+    );
+  };
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
   render() {
     return (
       <div className="homePage">
-        <Banner handleInput={this.handleInput} />
+        <Banner
+          handleInput={this.handleInput}
+          toggleClickHandler={this.toggleClickHandler}
+          sideDrawerOpen={this.state.sideDrawerOpen}
+          width={this.state.width}
+        />
         <Link to="/test">
           Test
           {auth.isAuthenticated()}
         </Link>
+        {this.state.sideDrawerOpen && this.state.width <= 570 ? (
+          <MobileMenuLinks
+            sideDrawerOpen={this.state.sideDrawerOpen}
+            width={this.state.width}
+          />
+        ) : null}
       </div>
     );
   }
